74ad97c3079d77b690874a65d1f753cedff45cfd
Joe Wainer <91285524+JoeWainer@users.noreply.github.com>
2021-12-12 22:14:44 +0200

    Create style.cc

---
+++ style.cc
@@ -0,0 +1,36 @@
+@import url('https://fonts.googleapis.com/css?family=Press+Start+2P|Rock+Salt|Roboto+Mono|Aldrich');
+
+h1 {
+ font-family: "Aldrich";
+ font-size: 24px;
+ background-color: #01DFD7;
+ line-height: 150%;
+}
+
+p {
+ line-height: 70%;
+ font-family: "Roboto Mono";
+ font-size: 13px;
+}
+
+button {
+ line-height: 200%;
+ font-family: "Aldrich";
+ font-size: 18px;
+}
+
+input {
+ line-height: 200%;
+ font-family: "Roboto Mono";
+ font-size: 18px;
+ margin-left: 5px;
+ width: 400px;
+}
+
+.hints {
+ font-family: "Roboto Mono";
+ font-size: 12px;
+ list-style-type: square;
+ margin-top: 0px;
+ padding-left: 30px;
+}
